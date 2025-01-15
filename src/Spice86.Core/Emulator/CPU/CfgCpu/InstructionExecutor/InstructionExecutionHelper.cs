OpenRakis/Spice86

Call flow handler for CFG CPU

src/Spice86.Core/Emulator/CPU/CfgCpu/InstructionExecutor/InstructionExecutionHelper.cs
@@ -19,15 +19,19 @@ using Spice86.Shared.Emulator.Memory;
 using Spice86.Shared.Interfaces;
 using Spice86.Shared.Utils;
 
+using System.Runtime.CompilerServices;
+
 public class InstructionExecutionHelper {
     private readonly ILoggerService _loggerService;
     private readonly BreakPointHolder _interruptBreakPoints;
+    private readonly ExecutionContextManager _executionContextManager;
 
     public InstructionExecutionHelper(State state,
         IMemory memory,
         IOPortDispatcher ioPortDispatcher,
         CallbackHandler callbackHandler,
         BreakPointHolder interruptBreakPoints,
+        ExecutionContextManager executionContextManager,
         ILoggerService loggerService) {
         _loggerService = loggerService;
         State = state;
@@ -40,6 +44,7 @@ public class InstructionExecutionHelper {
         IoPortDispatcher = ioPortDispatcher;
         CallbackHandler = callbackHandler;
         _interruptBreakPoints = interruptBreakPoints;
+        _executionContextManager = executionContextManager;
         InstructionFieldValueRetriever = new(memory);
         ModRm = new(state, memory, InstructionFieldValueRetriever);
     }
@@ -57,6 +62,8 @@ public class InstructionExecutionHelper {
     public UInt16RegistersIndexer UInt16Registers => State.GeneralRegisters.UInt16;
     public UInt32RegistersIndexer UInt32Registers => State.GeneralRegisters.UInt32;
     public UInt16RegistersIndexer SegmentRegisters => State.SegmentRegisters.UInt16;
+    private FunctionHandler CurrentFunctionHandler => _executionContextManager.CurrentExecutionContext.FunctionHandler;
+
     public ICfgNode? NextNode { get; set; }
     
     public ushort SegmentValue(IInstructionWithSegmentRegisterIndex instruction) {
@@ -95,45 +102,47 @@ public class InstructionExecutionHelper {
 
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void NearCallOffset(CfgInstruction instruction, int offset) {
         MoveIpToEndOfInstruction(instruction);
         ushort callIP = (ushort)(State.IP + offset);
         NearCall(instruction, State.IP, callIP);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void NearCallWithReturnIpNextInstruction(CfgInstruction instruction, ushort callIP) {
         MoveIpToEndOfInstruction(instruction);
         NearCall(instruction, State.IP, callIP);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void NearCall(CfgInstruction instruction, ushort returnIP, ushort callIP) {
         Stack.Push16(returnIP);
-        HandleCall(instruction, CallType.NEAR, State.CS, returnIP, State.CS, callIP);
+        HandleCall(instruction, CallType.NEAR, new SegmentedAddress(State.CS, returnIP),  new SegmentedAddress(State.CS, callIP));
     }
 
-    public void FarCallWithReturnIpNextInstruction(CfgInstruction instruction, ushort targetCS, ushort targetIP) {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void FarCallWithReturnIpNextInstruction(CfgInstruction instruction, SegmentedAddress target) {
         MoveIpToEndOfInstruction(instruction);
-        FarCall(instruction, State.CS, State.IP, targetCS, targetIP);
+        FarCall(instruction, State.IpSegmentedAddress, target);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void FarCall(CfgInstruction instruction,
-        ushort returnCS,
-        ushort returnIP,
-        ushort targetCS,
-        ushort targetIP) {
-        Stack.Push16(returnCS);
-        Stack.Push16(returnIP);
-        HandleCall(instruction, CallType.FAR, returnCS, returnIP, targetCS, targetIP);
+        SegmentedAddress returnAddress,
+        SegmentedAddress target) {
+        Stack.PushSegmentedAddress(returnAddress);
+        HandleCall(instruction, CallType.FAR, returnAddress, target);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void HandleCall(CfgInstruction instruction,
         CallType callType,
-        ushort returnCS,
-        ushort returnIP,
-        ushort targetCS,
-        ushort targetIP) {
-        State.CS = targetCS;
-        State.IP = targetIP;
+        SegmentedAddress returnAddress,
+        SegmentedAddress target) {
+        State.CS = target.Segment;
+        State.IP = target.Offset;
+        CurrentFunctionHandler.Call(callType, target, returnAddress, instruction);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
@@ -147,8 +156,10 @@ public class InstructionExecutionHelper {
         HandleInterruptCall(instruction, vectorNumber);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void HandleInterruptCall(CfgInstruction instruction, byte vectorNumber) {
-        DoInterrupt(vectorNumber);
+        (SegmentedAddress target, SegmentedAddress expectedReturn) = DoInterrupt(vectorNumber);
+        CurrentFunctionHandler.ICall(target, expectedReturn, instruction, vectorNumber, false);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
     
@@ -161,30 +172,33 @@ public class InstructionExecutionHelper {
         }
         SegmentedAddress expectedReturn = State.IpSegmentedAddress;
         Stack.Push16(State.Flags.FlagRegister16);
-        Stack.Push16(State.CS);
-        Stack.Push16(State.IP);
+        Stack.PushSegmentedAddress(expectedReturn);
         State.InterruptFlag = false;
         State.IP = target.Offset;
         State.CS = target.Segment;
         return (target, expectedReturn);
     }
-    
-    public void HandleInterruptRet(CfgInstruction instruction) {
-        State.IP = Stack.Pop16();
-        State.CS = Stack.Pop16();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void HandleInterruptRet<T>(T instruction) where T : CfgInstruction, IReturnInstruction {
+        CurrentFunctionHandler.Ret(CallType.INTERRUPT, instruction);
+        State.IpSegmentedAddress = Stack.PopSegmentedAddress();
         State.Flags.FlagRegister = Stack.Pop16();
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
-    public void HandleNearRet(CfgInstruction instruction, int numberOfBytesToPop = 0) {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void HandleNearRet<T>(T instruction, int numberOfBytesToPop = 0) where T : CfgInstruction, IReturnInstruction {
+        CurrentFunctionHandler.Ret(CallType.NEAR, instruction);
         State.IP = Stack.Pop16();
         Stack.Discard(numberOfBytesToPop);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
-    public void HandleFarRet(CfgInstruction instruction, int numberOfBytesToPop = 0) {
-        State.IP = Stack.Pop16();
-        State.CS = Stack.Pop16();
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void HandleFarRet<T>(T instruction, int numberOfBytesToPop = 0) where T : CfgInstruction, IReturnInstruction {
+        CurrentFunctionHandler.Ret(CallType.FAR, instruction);
+        State.IpSegmentedAddress = Stack.PopSegmentedAddress();
         Stack.Discard(numberOfBytesToPop);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/FarRet.cs
@@ -1,12 +1,16 @@
 ﻿namespace Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions;
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class FarRet : CfgInstruction {
+public class FarRet : CfgInstruction, IReturnInstruction {
 
     public FarRet(SegmentedAddress address, InstructionField<ushort> opcodeField) : base(address, opcodeField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
+
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleFarRet(this, 0);
     }

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/FarRetImm.cs
@@ -2,12 +2,15 @@
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.CommonGrammar;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class FarRetImm : InstructionWithValueField<ushort> {
+public class FarRetImm : InstructionWithValueField<ushort>, IReturnInstruction {
 
     public FarRetImm(SegmentedAddress address, InstructionField<ushort> opcodeField, InstructionField<ushort> valueField) : base(address, opcodeField, valueField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
 
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleFarRet(this, helper.InstructionFieldValueRetriever.GetFieldValue(ValueField));

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/NearRetImm.cs
@@ -2,12 +2,15 @@
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.CommonGrammar;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class NearRetImm : InstructionWithValueField<ushort> {
+public class NearRetImm : InstructionWithValueField<ushort>, IReturnInstruction {
 
     public NearRetImm(SegmentedAddress address, InstructionField<ushort> opcodeField, InstructionField<ushort> valueField) : base(address, opcodeField, valueField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
 
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleNearRet(this, helper.InstructionFieldValueRetriever.GetFieldValue(ValueField));

OpenRakis/Ix86

src/Spice86.Core/Emulator/CPU/CfgCpu/InstructionExecutor/InstructionExecutionHelper.cs
@@ -2,6 +2,7 @@ namespace Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 
 using Serilog.Events;
 
+using Spice86.Core.Emulator.CPU.CfgCpu.CallFlow;
 using Spice86.Core.Emulator.CPU.CfgCpu.ControlFlowGraph;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.CommonGrammar;
@@ -10,7 +11,6 @@ using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Prefix;
 using Spice86.Core.Emulator.CPU.Exceptions;
 using Spice86.Core.Emulator.CPU.Registers;
 using Spice86.Core.Emulator.Errors;
-using Spice86.Core.Emulator.Function;
 using Spice86.Core.Emulator.InterruptHandlers.Common.Callback;
 using Spice86.Core.Emulator.IOPorts;
 using Spice86.Core.Emulator.Memory;
@@ -20,12 +20,15 @@ using Spice86.Shared.Utils;
 
 public class InstructionExecutionHelper {
     private readonly ILoggerService _loggerService;
-
+    private readonly ExecutionContextManager _executionContextManager;
     public InstructionExecutionHelper(State state,
         IMemory memory,
         IOPortDispatcher ioPortDispatcher,
-        CallbackHandler callbackHandler, ILoggerService loggerService) {
+        CallbackHandler callbackHandler,
+        ExecutionContextManager executionContextManager,
+        ILoggerService loggerService) {
         _loggerService = loggerService;
+        _executionContextManager = executionContextManager;
         State = state;
         Memory = memory;
         InterruptVectorTable = new(memory);
@@ -52,6 +55,8 @@ public class InstructionExecutionHelper {
     public UInt16RegistersIndexer UInt16Registers => State.GeneralRegisters.UInt16;
     public UInt32RegistersIndexer UInt32Registers => State.GeneralRegisters.UInt32;
     public UInt16RegistersIndexer SegmentRegisters => State.SegmentRegisters.UInt16;
+    private CallFlowHandler CurrentCallFlowHandler => _executionContextManager.CurrentExecutionContext.CallFlowHandler;
+
     public ICfgNode? NextNode { get; set; }
     
     public ushort SegmentValue(IInstructionWithSegmentRegisterIndex instruction) {
@@ -103,32 +108,28 @@ public class InstructionExecutionHelper {
 
     public void NearCall(CfgInstruction instruction, ushort returnIP, ushort callIP) {
         Stack.Push16(returnIP);
-        HandleCall(instruction, CallType.NEAR, State.CS, returnIP, State.CS, callIP);
+        HandleCall(instruction, CallType.NEAR, new SegmentedAddress(State.CS, returnIP),  new SegmentedAddress(State.CS, callIP));
     }
 
-    public void FarCallWithReturnIpNextInstruction(CfgInstruction instruction, ushort targetCS, ushort targetIP) {
+    public void FarCallWithReturnIpNextInstruction(CfgInstruction instruction, SegmentedAddress target) {
         MoveIpToEndOfInstruction(instruction);
-        FarCall(instruction, State.CS, State.IP, targetCS, targetIP);
+        FarCall(instruction, State.IpSegmentedAddress, target);
     }
 
     public void FarCall(CfgInstruction instruction,
-        ushort returnCS,
-        ushort returnIP,
-        ushort targetCS,
-        ushort targetIP) {
-        Stack.Push16(returnCS);
-        Stack.Push16(returnIP);
-        HandleCall(instruction, CallType.FAR, returnCS, returnIP, targetCS, targetIP);
+        SegmentedAddress returnAddress,
+        SegmentedAddress target) {
+        Stack.PushSegmentedAddress(returnAddress);
+        HandleCall(instruction, CallType.FAR, returnAddress, target);
     }
 
     public void HandleCall(CfgInstruction instruction,
         CallType callType,
-        ushort returnCS,
-        ushort returnIP,
-        ushort targetCS,
-        ushort targetIP) {
-        State.CS = targetCS;
-        State.IP = targetIP;
+        SegmentedAddress returnAddress,
+        SegmentedAddress target) {
+        State.CS = target.Segment;
+        State.IP = target.Offset;
+        CurrentCallFlowHandler.Call(callType, target, returnAddress, instruction);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
@@ -143,7 +144,8 @@ public class InstructionExecutionHelper {
     }
 
     public void HandleInterruptCall(CfgInstruction instruction, byte vectorNumber) {
-        DoInterrupt(vectorNumber);
+        (SegmentedAddress target, SegmentedAddress expectedReturn) = DoInterrupt(vectorNumber);
+        CurrentCallFlowHandler.Call(CallType.INTERRUPT, target, expectedReturn, instruction);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
     
@@ -155,30 +157,30 @@ public class InstructionExecutionHelper {
         }
         SegmentedAddress expectedReturn = State.IpSegmentedAddress;
         Stack.Push16(State.Flags.FlagRegister16);
-        Stack.Push16(State.CS);
-        Stack.Push16(State.IP);
+        Stack.PushSegmentedAddress(expectedReturn);
         State.InterruptFlag = false;
         State.IP = target.Offset;
         State.CS = target.Segment;
         return (target, expectedReturn);
     }
     
-    public void HandleInterruptRet(CfgInstruction instruction) {
-        State.IP = Stack.Pop16();
-        State.CS = Stack.Pop16();
+    public void HandleInterruptRet(IRetInstruction instruction) {
+        CurrentCallFlowHandler.Ret(CallType.INTERRUPT, instruction);
+        State.IpSegmentedAddress = Stack.PopSegmentedAddress();
         State.Flags.FlagRegister = Stack.Pop16();
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
-    public void HandleNearRet(CfgInstruction instruction, int numberOfBytesToPop = 0) {
+    public void HandleNearRet(IRetInstruction instruction, int numberOfBytesToPop = 0) {
+        CurrentCallFlowHandler.Ret(CallType.NEAR, instruction);
         State.IP = Stack.Pop16();
         Stack.Discard(numberOfBytesToPop);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
 
-    public void HandleFarRet(CfgInstruction instruction, int numberOfBytesToPop = 0) {
-        State.IP = Stack.Pop16();
-        State.CS = Stack.Pop16();
+    public void HandleFarRet(IRetInstruction instruction, int numberOfBytesToPop = 0) {
+        CurrentCallFlowHandler.Ret(CallType.FAR, instruction);
+        State.IpSegmentedAddress = Stack.PopSegmentedAddress();
         Stack.Discard(numberOfBytesToPop);
         SetNextNodeToSuccessorAtCsIp(instruction);
     }
@@ -187,12 +189,13 @@ public class InstructionExecutionHelper {
         State.IP = (ushort)(State.IP + instruction.Length);
     }
 
-    public ICfgNode? GetSuccessorAtCsIp(CfgInstruction instruction) {
+    public ICfgNode? GetSuccessorAtCsIp(ICfgInstruction instruction) {
         instruction.SuccessorsPerAddress.TryGetValue(State.IpSegmentedAddress, out ICfgNode? res);
         return res;
     }
 
-    public void SetNextNodeToSuccessorAtCsIp(CfgInstruction instruction) {
+    public void SetNextNodeToSuccessorAtCsIp(ICfgInstruction instruction) {
+        _loggerService.LoggerPropertyBag.CsIp = State.IpSegmentedAddress;
         NextNode = GetSuccessorAtCsIp(instruction);
     }
 

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/FarRet.cs
@@ -1,12 +1,16 @@
 ﻿namespace Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions;
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class FarRet : CfgInstruction {
+public class FarRet : CfgInstruction, IRetInstruction {
 
     public FarRet(SegmentedAddress address, InstructionField<ushort> opcodeField) : base(address, opcodeField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
+
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleFarRet(this, 0);
     }

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/FarRetImm.cs
@@ -2,12 +2,15 @@
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.CommonGrammar;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class FarRetImm : InstructionWithValueField<ushort> {
+public class FarRetImm : InstructionWithValueField<ushort>, IRetInstruction {
 
     public FarRetImm(SegmentedAddress address, InstructionField<ushort> opcodeField, InstructionField<ushort> valueField) : base(address, opcodeField, valueField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
 
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleFarRet(this, helper.InstructionFieldValueRetriever.GetFieldValue(ValueField));

src/Spice86.Core/Emulator/CPU/CfgCpu/ParsedInstruction/Instructions/NearRetImm.cs
@@ -2,12 +2,15 @@
 
 using Spice86.Core.Emulator.CPU.CfgCpu.InstructionExecutor;
 using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.CommonGrammar;
+using Spice86.Core.Emulator.CPU.CfgCpu.ParsedInstruction.Instructions.Interfaces;
 using Spice86.Shared.Emulator.Memory;
 
-public class NearRetImm : InstructionWithValueField<ushort> {
+public class NearRetImm : InstructionWithValueField<ushort>, IRetInstruction {
 
     public NearRetImm(SegmentedAddress address, InstructionField<ushort> opcodeField, InstructionField<ushort> valueField) : base(address, opcodeField, valueField) {
     }
+
+    public CfgInstruction? CurrentCorrespondingCallInstruction { get; set; }
 
     public override void Execute(InstructionExecutionHelper helper) {
         helper.HandleNearRet(this, helper.InstructionFieldValueRetriever.GetFieldValue(ValueField));